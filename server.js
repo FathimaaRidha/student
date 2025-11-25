529148a96294492b05dd61b42459fdab7c68a837
FathimaaRidha <fathimaridha.cs@gmail.com>
2025-11-26 04:41:36 +0530

    second commit

--- server.js
+++ server.js
@@ -23,14 +23,26 @@ function readDataFile() {
 function writeDataFile(data) {
     fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
 }   
-// add a new student
+// add a new task
 app.post('/students', (req, res) => {
+    const{name, age, course, year, status } = req.body;
+    if(!name || !age || !course || !year || !status) {
+        return  res.status(400).json({error: 'All fields are required'});
+    }   
     const students = readDataFile();
-    const newStudent = { id: uuid.v4(), ...req.body };  
+    const newStudent = {
+        id: uuid.v4(),
+        name,
+        age,
+        course,
+        year,
+        status
+    };
     students.push(newStudent);
     writeDataFile(students);
     res.status(201).json(newStudent);
 });
+
 // get all students
 app.get('/students', (req, res) => {
     const students = readDataFile();
